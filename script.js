74de5a1ab92307d98fc95ff05f51e9258c41afac
Kim Oflash <105842823+kimoflash@users.noreply.github.com>
2023-01-11 18:03:05 +0800

    Function for rock-paper-scissors game

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const choices = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
     return choices[~~(Math.random() * choices.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice().toLowerCase();
+
+    if (playerSelection == computerSelection) {
+        return `It's a tie! You both chose ${playerSelection.toUpperCase()}.`;
+    } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
+        return `You win! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} beats ${computerSelection}.`;
+    } else {
+        return `You lose! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} is weak against ${computerSelection}.`;
+    };
+}
+
+/*const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));*/
+
