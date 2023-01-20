f376fe08c96db9b3670625a95432d86465acc638
Kim Oflash <105842823+kimoflash@users.noreply.github.com>
2023-01-20 16:57:44 +0800

    Add score tracker for player and computer which increments per round

--- script.js
+++ script.js
@@ -7,26 +7,64 @@ function getComputerChoice() {
 
 let getPlayerChoice = prompt("Which weapon would you choose? Rock, Paper, or Scissors?");
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = getPlayerChoice.toLowerCase();
     computerSelection = getComputerChoice().toLowerCase();
-
+    
     if (playerSelection == computerSelection) {
-        return `It's a tie! You both chose ${playerSelection.toUpperCase()}.`;
-    } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
-        return `You win! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} beats ${computerSelection}.`;
+        playerScore++;
+        computerScore++;
+        return `It's a tie! You both chose ${playerSelection.toUpperCase()}. Your Score: ${playerScore}, Computer Score: ${computerScore}`;
+    } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") || 
+        (playerSelection == "paper" && computerSelection == "rock") || 
+        (playerSelection == "scissors" && computerSelection == "paper")
+        ) {
+        playerScore++;
+        return `You win! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} beats ${computerSelection}. Your Score: ${playerScore}, Computer Score: ${computerScore}`;
     } else {
-        return `You lose! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} is weak against ${computerSelection}.`;
-    };
+        computerScore++;
+        return `You lose! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} is weak against ${computerSelection}. Your Score: ${playerScore}, Computer Score: ${computerScore}`;    
+    }
 }
 
 console.log(playRound());
+console.log(playRound());
+console.log(playRound());
+console.log(playRound());
+console.log(playRound());
+
+/*function game() {
+
+    for (let i = 0; i < 5; i++) {
+        if (i == 1) {
+            playRound();
+            
+        }
+    } 
+
+}*/
+
+/*function getScore() {
+    if (playerSelection == computerSelection) {
+        playerScore += 1;
+        computerScore += 1;
+        return `It's a tie! Player's Score: ${playerScore} Computer's Score: ${computerScore}.`
+    } else if (playerSelection == "rock" && computerSelection == "scissors" || 
+    playerSelection == "paper" && computerSelection == "rock" || 
+    playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore += 1;
+        return `Player's Score: ${playerScore} Computer's Score: ${computerScore}.`
+    } else {
+        computerScore += 1;
+        return `Player's Score: ${playerScore} Computer's Score: ${computerScore}.`
+    }
+
+}*/
 
 /*const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));*/
-
-/*function game() {
-
-}*/
-
