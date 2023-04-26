8ded82e4463928a6ea7f6e8dfa7036a44720910b
Kim Oflash <105842823+kimoflash@users.noreply.github.com>
2023-04-26 15:55:47 +0800

    When buttons are clicked, it will show a message if the player won or loses

--- script.js
+++ script.js
@@ -5,34 +5,66 @@ function getComputerChoice() {
     return choices[~~(Math.random() * choices.length)];
 }
 
-let playerScore = 0;
-let computerScore = 0;
+var playerScore = 0;
+var computerScore = 0;
+
+const playerChoiceBtn = document.querySelectorAll('button');
+
+playerChoiceBtn.forEach((button) => {
+    button.addEventListener('click', function (e){
+        playRound(e.target.textContent, getComputerChoice())
+    });
+
+    // THIS IS THE SHORT FORM
+    /*
+    button.addEventListener('click', (e) => {
+        playRound(e.target.textContent, getComputerChoice())
+    });
+    */ 
+});
 
 function playRound(playerSelection, computerSelection) {
-    let getPlayerChoice = prompt("Which weapon would you choose? Rock, Paper, or Scissors?");
-
-    playerSelection = getPlayerChoice.toLowerCase();
-    computerSelection = getComputerChoice().toLowerCase();
-    
-    if (playerSelection == computerSelection) {
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
+ 
+    if (playerChoice == computerChoice) {
         playerScore++;
         computerScore++;
-        return `It's a tie! You both chose ${playerSelection.toUpperCase()}. Your Score: ${playerScore}, Computer Score: ${computerScore}`;
+        const tie = `It's a tie! You both chose ${playerSelection.toUpperCase()}. Your Score: ${playerScore}, Computer Score: ${computerScore}`;
+        showResult(tie);
     } else if (
-        (playerSelection == "rock" && computerSelection == "scissors") || 
-        (playerSelection == "paper" && computerSelection == "rock") || 
-        (playerSelection == "scissors" && computerSelection == "paper")
+        (playerChoice == "rock" && computerChoice == "scissors") || 
+        (playerChoice == "paper" && computerChoice == "rock") || 
+        (playerChoice == "scissors" && computerChoice == "paper")
         ) {
         playerScore++;
-        return `You win! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} beats ${computerSelection}. Your Score: ${playerScore}, Computer Score: ${computerScore}`;
+        const win = `You win! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} beats ${computerSelection}. Your Score: ${playerScore}, Computer Score: ${computerScore}`;
+        showResult(win);
     } else {
         computerScore++;
-        return `You lose! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} is weak against ${computerSelection}. Your Score: ${playerScore}, Computer Score: ${computerScore}`;    
+        const lose = `You lose! ${playerSelection[0].toUpperCase() + playerSelection.substring(1)} is weak against ${computerSelection}. Your Score: ${playerScore}, Computer Score: ${computerScore}`;
+        showResult(lose);
     }
 
 }
 
-playRound();
+function showResult(message) {
+    const result = document.getElementById('result');
+    result.textContent = message;
+}
+
+
+// THIS IS NOT YET WORKING! SHOULD BE SHOWING THE WINNER THAT SCORES 5 FIRST
+function showWinner(playerScore, computerScore) {
+    const playerWins = "ALL HAIL! YOU WIN! Hit F5 to play one more round!";
+    const computerWins = "AW, SNAP! COMPUTER WINS. Hit F5 to play one more round!";
+
+    if (playerScore = 5) {
+        winnerResult.textContent = playerWins;
+    } else if (computerScore == 5) {
+        winnerResult.textContent = computerWins;
+    }
+}
 
 /*
 function game() {
@@ -49,7 +81,4 @@ function game() {
         }
      }
 }
-
-game();
-*/
-
+*/ 
